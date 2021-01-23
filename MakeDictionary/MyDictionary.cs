5c638f515d3bc1766892e03ed307ecf059c71a9e
sumeyyenurozgenc <sumeyyenurozgenc@gmail.com>
2021-01-23 16:22:20 +0300

    Add project files.

---
+++ MakeDictionary/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakeDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _key;
+        TValue[] _value;
+        public MyDictionary()
+        {
+            _key = new TKey[0];
+            _value = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKey = _key;
+            TValue[] tempValue = _value;
+
+            _key = new TKey[_key.Length + 1];
+            _value = new TValue[_value.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                _key[i] = tempKey[i];
+                _value[i] = tempValue[i];
+            }
+
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+
+        }
+    }
+}
---
+++ MakeDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MakeDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "C# Kursu");
+            myDictionary.Add(2, "Java Kursu");
+            myDictionary.Add(3, "SQL Kursu");
+
+        }
+    }
+}
